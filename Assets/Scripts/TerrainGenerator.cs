e9bab453eae3268fbd3cd44a10b1c55cfbb09a45
Jakub Kotwica <01.jakub.kotwica@gmail.com>
2025-05-04 17:30:49 +0200

    new generator

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -5,8 +5,10 @@ public class TerrainGenerator : MonoBehaviour
     public int width = 256;
     public int depth = 256;
     public int height = 20;
-    public float scale = 20f;
-    
+    public float scale1 = 20f;
+    public float scale2 = 5f;
+    public float influence = 0.5f;
+
     private float offsetX;
     private float offsetZ;
 
@@ -14,7 +16,6 @@ public class TerrainGenerator : MonoBehaviour
     {
         offsetX = Random.Range(0f, 10000f);
         offsetZ = Random.Range(0f, 10000f);
-
         GenerateTerrain();
     }
 
@@ -40,18 +41,29 @@ public class TerrainGenerator : MonoBehaviour
         {
             for (int z = 0; z < depth; z++)
             {
-                float xCoord = (float)x / width * scale + offsetX;
-                float zCoord = (float)z / depth * scale + offsetZ;
-                float perlinValue = Mathf.PerlinNoise(xCoord, zCoord);
+                float xCoord1 = (float)x / width * scale1 + offsetX;
+                float zCoord1 = (float)z / depth * scale1 + offsetZ;
+                float perlinValue1 = Mathf.PerlinNoise(xCoord1, zCoord1);
 
-                if (perlinValue < 0.3f)
+                float xCoord2 = (float)x / width * scale2 + offsetX;
+                float zCoord2 = (float)z / depth * scale2 + offsetZ;
+                float perlinValue2 = Mathf.PerlinNoise(xCoord2, zCoord2);
+
+                float combinedHeight = Mathf.Lerp(perlinValue1, perlinValue2, influence);
+
+                if (combinedHeight < 0.3f)
                 {
-                    heights[x, z] = perlinValue * 0.2f;
+                    combinedHeight *= 0.2f;
                 }
-                else
+
+                if (combinedHeight > 0.7f)
                 {
-                    heights[x, z] = perlinValue;
+                    float t = (combinedHeight - 0.7f) / (1.0f - 0.7f);
+                    float modified = Mathf.Pow(combinedHeight, 1.5f);
+                    combinedHeight = Mathf.Lerp(combinedHeight, modified, t);
                 }
+
+                heights[x, z] = combinedHeight;
             }
         }
         return heights;
